6360b0121128c00e1b6249ae2af87f96b9cdb014
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-06-08 22:02:56 +0200

    Human abstract class - apply Personality, Location, Abilities & equals method

--- src/main/java/dawid/luczak/model/human/Human.java
+++ src/main/java/dawid/luczak/model/human/Human.java
@@ -1,10 +1,18 @@
 package dawid.luczak.model.human;
 
-import dawid.luczak.contract.HumanBeing;
+import dawid.luczak.contract.human.HumanBeing;
+import dawid.luczak.model.LocationEnum;
+import dawid.luczak.model.ability.HumanAbilities;
 import dawid.luczak.model.human.baby.Baby;
+import dawid.luczak.model.organism.Organism;
 
-public abstract class Human extends Personality implements HumanBeing
+import java.util.Objects;
+
+public abstract class Human extends Organism implements HumanBeing
 {
+	private final Personality personality;
+	private LocationEnum location;
+	private final HumanAbilities abilities;
 	
 	public static Human create(){
 		return new Human() {};
@@ -12,10 +20,23 @@ public abstract class Human extends Personality implements HumanBeing
 	
 	protected Human(){
 		super();
+		personality = new Personality(this, true);
+		location = LocationEnum.SEA;
+		abilities = new HumanAbilities();
+	}
+	
+	protected Human(String firstName, String lastName){
+		super();
+		personality = new Personality(this, firstName, lastName);
+		location = LocationEnum.SEA;
+		abilities = new HumanAbilities();
 	}
 	
 	protected Human(Human source){
-		super(source);
+		super();
+		personality = new Personality(source);
+		location = source.location;
+		abilities = source.abilities;
 	}
 	
 	@Override
@@ -28,10 +49,43 @@ public abstract class Human extends Personality implements HumanBeing
 		return Baby.create();
 	}
 	
+	@Override
+	public void die() {
+
+	}
 	
 	@Override
 	public boolean isAlive() {
 		return !(this instanceof Corps);
+	}
+	
+	@Override
+	public Personality getPersonality() {
+		return personality;
+	}
+	
+	
+	@Override
+	public Human getFemale() {
+		return null;
+	}
+	
+	@Override
+	public Human getMale() {
+		return null;
+	}
+	
+	@Override
+	public void setLocation(LocationEnum location) {
+		this.location = location;
+	}
+	
+	public LocationEnum getLocation() {
+		return location;
+	}
+	
+	public HumanAbilities getAbilities() {
+		return abilities;
 	}
 	
 	/*	OTHER  */
@@ -53,9 +107,22 @@ public abstract class Human extends Personality implements HumanBeing
 	
 	@Override
 	public String toString() {
-		return getName() + "\n" +
-							 getGenderString() + "\n" +
-							 getBirthDate().toLocalDate() + ", age: " + getAge() + "\n"
-							 + "Adult?: " + isAdult() + "\n";
+		return personality.toString();
 	}
+	
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Human)) return false;
+		Human human = (Human) o;
+		return getPersonality().equals(human.getPersonality()) && getLocation().equals(human.getLocation()) && getAbilities().equals(human.abilities);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(getPersonality(), getLocation(), getAbilities());
+	}
+	
+	
 }
