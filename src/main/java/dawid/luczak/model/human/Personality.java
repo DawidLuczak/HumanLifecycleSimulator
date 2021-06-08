b88e78464233a72bb2f3c70edaad13fde63f192b
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-06-08 22:04:52 +0200

    Personality class - extended by human methods, constructors fixed

--- src/main/java/dawid/luczak/model/human/Personality.java
+++ src/main/java/dawid/luczak/model/human/Personality.java
@@ -1,100 +1,134 @@
 package dawid.luczak.model.human;
 
-import dawid.luczak.contract.*;
+import dawid.luczak.contract.human.Female;
+import dawid.luczak.contract.human.Male;
+import dawid.luczak.contract.human.personality.PersonalityInterface;
 import dawid.luczak.model.human.adult.Adult;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public abstract class Personality implements Name, Age, Gender {
-	private String name;
-	private final LocalDateTime dateOfBirth;
+public class Personality implements PersonalityInterface {
 	
-	Personality(){
-		dateOfBirth = LocalDateTime.now();
+	private final List<String> names;
+	private final LocalDateTime dateOfBirth;
+	private Human human;
+	
+	Personality(Human human, Boolean empty){
+		this.dateOfBirth = LocalDateTime.now();
+		this.names = new ArrayList<>();
+		this.human = human;
+	}
+	
+	Personality(Human human, String firstName, String lastName){
+		this.dateOfBirth = LocalDateTime.now();
+		this.names = new ArrayList<>();
+		this.names.add(firstName);
+		this.names.add(lastName);
+		this.human = human;
 	}
 	
 	Personality(Human human){
-		this.name = human.getName();
-		this.dateOfBirth = human.getBirthDate();
+		this.dateOfBirth = LocalDateTime.of(human.getPersonality().dateOfBirth.toLocalDate(), human.getPersonality().dateOfBirth.toLocalTime());
+		this.names = new ArrayList<>();
+		this.names.addAll(List.copyOf(human.getPersonality().names));
 	}
 	
+	Personality(Personality personality){
+		this.dateOfBirth = personality.getBirthDate();
+		this.names = personality.getNames();
+		this.human = personality.human;
+	}
+	
+	
+	public Personality copyTo(Human human){
+		Personality copy = new Personality(this);
+		copy.setHuman(human);
+		return copy;
+	}
 	
 /*	NAMES  */
-	@Override
-	public String getName() {
-		return name;
+	
+	
+	public void setHuman(Human human) {
+		this.human = human;
 	}
 	
 	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	@Override
-	public void setNames(Human source) {
-		setFirstName(source.getFirstName());
-		setSecondName(source.getSecondName());
-		setThirdName(source.getThirdName());
-		setLastName(source.getLastName());
-		nameUpdate();
-	}
-	
-	@Override
-	public void nameUpdate() {
-		name = "";
-		if (getFirstName() != null)
-			name += getFirstName();
-		if (getSecondName() != null)
-			name += " " + getSecondName();
-		if (getThirdName() != null)
-			name += " " + getThirdName();
-		if (getLastName() != null)
-			name += " " + getLastName();
+	public List<String> getNames() {
+		return names;
 	}
 	
 	@Override
 	public String getFirstName() {
-		return null;
+		if (names.size() > 0)
+			return names.get(0);
+		else
+			return "";
 	}
 	
 	@Override
 	public void setFirstName(String firstName) {
-		nameUpdate();
+		if (names.size() > 0)
+			names.set(0, firstName);
+		else
+			names.add(0, firstName);
 	}
 	
 	@Override
 	public String getSecondName() {
-		return null;
+		if (names.size() > 2)
+			return names.get(1);
+		else
+			return "";
 	}
 	
 	@Override
 	public void setSecondName(String secondName) {
-		nameUpdate();
+		if (names.size() > 2)
+			names.set(1, secondName);
+		else
+			names.add(1, secondName);
 	}
 	
 	@Override
 	public String getThirdName() {
-		return null;
+		if (names.size() > 3)
+			return names.get(2);
+		else
+			return "";
 	}
 	
 	@Override
 	public void setThirdName(String thirdName) {
-		nameUpdate();
+		if (names.size() > 3)
+			names.set(3, thirdName);
+		else
+			names.add(3, thirdName);
 	}
 	
 	@Override
 	public String getLastName() {
-		return null;
+		if (names.size() > 1)
+			return names.get(names.size() - 1);
+		else
+			return "";
 	}
 	
 	@Override
 	public void setLastName(String lastName) {
-		nameUpdate();
+		if (names.size() > 1)
+			names.set(names.size() - 1, lastName);
+		else
+			names.add(names.size(), lastName);
 	}
 	
 	
-/*	AGE  */
+	
+	
+	/*	AGE  */
 	
 	@Override
 	public LocalDateTime getBirthDate() {
@@ -108,7 +142,7 @@ public abstract class Personality implements Name, Age, Gender {
 	
 	@Override
 	public boolean isAdult() {
-	return this instanceof Adult;
+		return human instanceof Adult;
 	}
 	
 	@Override
@@ -116,23 +150,18 @@ public abstract class Personality implements Name, Age, Gender {
 	return 0;
 	}
 	
+	
 /*	GENDER  */
-	@Override
-	public char getGender() {
-		return isMale() ? 'm' : isFemale() ? 'f' : 'u';
-	}
 	
-	@Override
-	public void setGender(char gender) {}
-	
+
 	@Override
 	public boolean isFemale() {
-		return this instanceof Female;
+		return human instanceof Female;
 	}
 	
 	@Override
 	public boolean isMale() {
-		return this instanceof Male;
+		return human instanceof Male;
 	}
 	
 	@Override
@@ -145,9 +174,13 @@ public abstract class Personality implements Name, Age, Gender {
 		return null;
 	}
 	
-	@Override
-	public String getGenderString() {
-		return isMale() ? "male" : isFemale() ? "female" : "unknown";
+	public boolean compareGenders(Human other){
+		if (isMale())
+			return other.getPersonality().isMale();
+		else if (isFemale())
+			return other.getPersonality().isFemale();
+		else
+			return !other.getPersonality().isFemale() && !other.getPersonality().isMale();
 	}
 	
 	@Override
@@ -155,11 +188,30 @@ public abstract class Personality implements Name, Age, Gender {
 		if (this == o) return true;
 		if (!(o instanceof Personality)) return false;
 		Personality that = (Personality) o;
-		return Objects.equals(getName(), that.getName()) && Objects.equals(dateOfBirth, that.dateOfBirth);
+		return getNames().equals(that.getNames()) && Objects.equals(dateOfBirth, that.dateOfBirth) && compareGenders(that.human);
 	}
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(getName(), dateOfBirth);
+		return Objects.hash(getNames(), dateOfBirth, human);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder name = new StringBuilder();
+		
+		if (!getFirstName().isBlank())
+			name.append(getFirstName());
+		
+		if (!getSecondName().isBlank())
+			name.append(" ").append(getSecondName());
+		
+		if (!getThirdName().isBlank())
+			name.append(" ").append(getThirdName());
+		
+		if (!getLastName().isBlank())
+			name.append(" ").append(getLastName());
+		
+		return name.toString();
 	}
 }
