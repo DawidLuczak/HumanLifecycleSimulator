e1b34492ba21bdebe4bfccdf295cf0496d43eaf6
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-06-08 17:44:49 +0200

    init Body interface

---
+++ src/main/java/dawid/luczak/contract/body/BodyInterface.java
@@ -0,0 +1,13 @@
+package dawid.luczak.contract.body;
+
+public interface BodyInterface {
+	
+	double getWeight();
+	
+	void setWeight(double weight);
+	
+	double getHeight();
+	
+	void setHeight(double height);
+	
+}
