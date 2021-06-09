894b22398adae1246664c4f35226940ed0fa9e36
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-06-09 17:32:14 +0200

    ActionButton class -

--- src/main/java/dawid/luczak/App.java
+++ src/main/java/dawid/luczak/App.java
@@ -1,9 +1,11 @@
 package dawid.luczak;
 
+import dawid.luczak.model.Controller;
+
 public class App
 {
     public static void main( String[] args )
     {
-    
+        new Controller();
     }
 }
