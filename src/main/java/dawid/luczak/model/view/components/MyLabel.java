ea4479f111f3a889f618ad3124179f68bb6a618e
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-06-09 17:17:33 +0200

    init MyLabel class - extends JLabel class - just default constructors

---
+++ src/main/java/dawid/luczak/model/view/components/MyLabel.java
@@ -0,0 +1,14 @@
+package dawid.luczak.model.view.components;
+
+import javax.swing.*;
+
+public class MyLabel extends JLabel {
+	
+	public MyLabel(){
+		super();
+	}
+	
+	public MyLabel(String text){
+		super(text);
+	}
+}
