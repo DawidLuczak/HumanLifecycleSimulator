a5a3b2f223e0e131239a1901060edfcdfb13a027
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-06-09 17:31:32 +0200

    Task class - should be abstract - allows to start the task or action

---
+++ src/main/java/dawid/luczak/model/Task.java
@@ -0,0 +1,18 @@
+package dawid.luczak.model;
+
+
+public class Task {
+	
+	
+	public Task(Controller controller){
+	
+	}
+	
+	public void start(Controller controller){
+	
+	}
+	
+	public String actionString(){
+		return "";
+	}
+}
